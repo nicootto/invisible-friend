3003f34a3d0daa81370e6c59a3beadae3df14b74
Nicolás Ottonello <nicolas.ottonello96@gmail.com>
2018-12-05 15:22:57 -0300

    Fix multiple join of same user to groups

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,7 +4,9 @@ class InvitationsController < ApplicationController
   def create
     invitation_code = params[:id]
     group = Group.find_by(invitation_code: invitation_code)
-    group.users << current_user
+    unless group.users.include? current_user
+      group.users << current_user
+    end
     redirect_to group
   end
 end
